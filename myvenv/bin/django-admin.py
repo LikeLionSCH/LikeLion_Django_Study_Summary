b892e6f536c845dbdede1fd97deaad4b99a43032
alstn2468 <alstn2468_@naver.com>
2019-01-09 23:54:06 +0900

    Complete OT

---
+++ myvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/blueprint/Documents/Like_Lion_7th/LikeLion_Study_Summary/myvenv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
