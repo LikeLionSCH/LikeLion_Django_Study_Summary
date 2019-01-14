579580d18c9c77c5538ff157a9ccd3ef8d403b1d
alstn2468 <alstn2468_@naver.com>
2019-01-14 22:03:00 +1000

    Complete Python_Challenge_115 chapter 8

--- Python_Challenge_115/8/6.py
+++ Python_Challenge_115/8/6.py
@@ -15,3 +15,19 @@ Example output
 * . . * . . *
 '''
 num = int(input())
+
+for i in range(num):
+    for j in range(num):
+        if i == j or (i + j) == (num - 1):
+            print('*', end=" ")
+
+        elif i == (num // 2):
+            print('*', end=" ")
+
+        elif j == (num // 2):
+            print('*', end=" ")
+
+        else:
+            print('.', end=" ")
+
+    print()
--- Python_Challenge_115/8/7.py
+++ Python_Challenge_115/8/7.py
@@ -13,3 +13,22 @@ Example output
 . * . * . * . *
 * . * . * . * .
 '''
+n, m = map(int, input().split())
+
+for i in range(n):
+    for j in range(m):
+        if i % 2 == 0:
+            if j % 2 == 0:
+                print('.', end=" ")
+
+            else:
+                print('*', end=" ")
+
+        else:
+            if j % 2 == 0:
+                print('*', end=" ")
+
+            else:
+                print(".", end=" ")
+
+    print()
