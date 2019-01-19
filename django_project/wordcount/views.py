60d0cb0172efe44846b1110b88a14ae510c8292a
alstn2468 <alstn2468_@naver.com>
2019-01-19 13:55:41 +0900

    Complete Week 2-3 wordcount part 2

--- django_project/wordcount/views.py
+++ django_project/wordcount/views.py
@@ -1,9 +1,30 @@
 from django.shortcuts import render
 
-# Create your views here.
 
 def home(request):
     return render(request, 'home.html')
 
+
 def about(request):
     return render(request, 'about.html')
+
+
+def result(request):
+    text = request.GET['fulltext']
+    words = text.split()
+    words_dic = {}
+
+    for word in words:
+        if word in words_dic.keys():
+            # Increase word count
+            words_dic[word] += 1
+
+        else:
+            # Add to dictionary
+            words_dic[word] = 1
+
+    return render(request, 'result.html',
+                  {'full': text,
+                   'total': len(words),
+                   'dictionary': words_dic.items(),
+                   })
