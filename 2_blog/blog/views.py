5ca9d97ce453e12154e9ffded0fdc2572802492a
alstn2468 <alstn2468_@naver.com>
2019-01-25 13:10:17 +0900

    Complete Week 3 - 3 queryset & method

--- 2_blog/blog/views.py
+++ 2_blog/blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Blog
 
-# Create your views here.
+def home(request):
+    blogs = Blog.objects
+
+    return render(request,
+                  'home.html',
+                  {'blogs': blogs})
--- 2_blog/secondproject/urls.py
+++ 2_blog/secondproject/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import blog.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', blog.views.home, name="home"),
 ]
