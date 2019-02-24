3e5eab8ceb46dd199c072ffba1a274c507671946
alstn2468 <alstn2468_@naver.com>
2019-02-25 06:38:46 +0900

    Using static, Seperate javascript code to static folder

--- 4_utilization/utilization/settings.py
+++ 4_utilization/utilization/settings.py
@@ -129,6 +129,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'accounts', 'static'),
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 LOGIN_REDIRECT_URL = '/'
 
